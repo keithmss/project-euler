9ceee52f784fca9c2d6a3c8e9a6ec34928515cf5
Keith Miller <keithms215@gmail.com>
2021-12-20 12:42:19 -0500

    euler5

---
+++ euler5/src/main.rs
@@ -0,0 +1,16 @@
+use rayon::prelude::*;
+
+static FACTOR: u32 = 20;
+
+fn main() {
+    // Find, in parallel, the first number that is divisible by 1..20.
+    let answer = (FACTOR..u32::MAX)
+        .into_par_iter()
+        .step_by(FACTOR as usize)
+        .find_first(|number| (2..FACTOR).all(|factor| number % factor == 0));
+
+    // Print the answer.
+    if let Some(a) = answer {
+        println!("{}", a);
+    }
+}
